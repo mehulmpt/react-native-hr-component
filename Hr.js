6b9a8de254d61ca4f9625cf83a48593982a5ea9f
Mehul Mohan <mehulmpt@gmail.com>
2018-01-29 19:22:01 +0530

    Fixed {[ error

--- Hr.js
+++ Hr.js
@@ -12,7 +12,7 @@ class Hr extends Component {
             { height: this.props.thickness || 1, backgroundColor: this.props.lineColor || "black" }
           ]}
         />
-        <Text style=[{this.props.fontSize && {fontSize: this.props.fontSize}}, {paddingLeft: this.props.textPadding || 3, paddingRight: this.props.textPadding || 3}, {...this.props.textStyles}]>{this.props.text}</Text>
+        <Text style={[this.props.fontSize && {fontSize: this.props.fontSize}, {paddingLeft: this.props.textPadding || 3, paddingRight: this.props.textPadding || 3}, this.props.textStyles]}>{this.props.text}</Text>
         <View
           style={[
             styles.side,
