415ce0b8e5841f805afa8b4efaa6430de985c1d4
Mehul Mohan <mehulmpt@gmail.com>
2018-01-29 19:25:33 +0530

    Included Text module

--- Hr.js
+++ Hr.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import styles from "./styles";
 import PropTypes from "prop-types";
 
