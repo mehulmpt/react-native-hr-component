69f3665e7d7a2468a8658de9592b4fe30a3c2a3e
Mehul Mohan <mehulmpt@gmail.com>
2019-07-25 21:11:15 +0530

    Version bump and minor typefixes

--- src.js
+++ src.js
@@ -3,28 +3,25 @@ import { View, Text, ViewPropTypes } from 'react-native'
 import styles from './styles'
 import PropTypes from 'prop-types'
 
-export default class Hr extends Component {
-	render() {
+function Hr(props) {
+	const { text, hrPadding, thickness, lineColor, fontSize, textPadding, hrStyles, textStyles } = props
 
-		const { text, hrPadding, thickness, lineColor, fontSize, textPadding, hrStyles, textStyles } = this.props
-
-		return (
-			<View style={[styles.row, { marginLeft: hrPadding, marginRight: hrPadding }, hrStyles]}>
-				<View style={[
+	return (
+		<View style={[styles.row, { marginLeft: hrPadding, marginRight: hrPadding }, hrStyles]}>
+			<View style={[
+				styles.side,
+				{ height: thickness, backgroundColor: lineColor }
+			]}
+			/>
+			<Text style={[fontSize && { fontSize: fontSize }, { paddingLeft: textPadding, paddingRight: textPadding }, textStyles]}>{text}</Text>
+			<View
+				style={[
 					styles.side,
 					{ height: thickness, backgroundColor: lineColor }
 				]}
-				/>
-				<Text style={[fontSize && { fontSize: fontSize }, { paddingLeft: textPadding, paddingRight: textPadding }, textStyles]}>{text}</Text>
-				<View
-					style={[
-						styles.side,
-						{ height: thickness, backgroundColor: lineColor }
-					]}
-				/>
-			</View>
-		)
-	}
+			/>
+		</View>
+	)
 }
 
 Hr.propTypes = {
@@ -37,8 +34,8 @@ Hr.propTypes = {
 	fontSize: PropTypes.number,
 	textPadding: PropTypes.number,
 	hrPadding: PropTypes.number,
-	hrStyles: ViewPropTypes.style,
-	textStyles: ViewPropTypes.style
+	hrStyles: PropTypes.object,
+	textStyles: PropTypes.object
 }
 
 Hr.defaultProps = {
@@ -47,3 +44,5 @@ Hr.defaultProps = {
 	textPadding: 3,
 	hrPadding: 0
 }
+
+export default Hr
